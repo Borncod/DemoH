261e8ce8b7c8d30d7bfb3781f21644fdbd2e105c
Borncod <qudusrasheed13@gmail.com>
2025-03-29 21:25:25 +0100

    added new files

--- include/action.php
+++ include/action.php
@@ -9,7 +9,7 @@ if($_SERVER["REQUEST_METHOD"]==="POST"){
     $dob = $_POST["dob"];
 }
 
-require_once "../DemoH/config/Dbh.php";
-require_once "../DemoH/config/signup.php";
+require_once("../DemoH/config/Dbh.php");
+require_once("../DemoH/config/signup.php");
 
 $signup = new Signup($username,$password,$user_image,$dob);
